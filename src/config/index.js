cd66acee4b8dd7108ea0888d8f4990068b71c489
Alejandro Rojas <alejandro@h2d.dev>
2024-07-10 04:40:17 -0400

    aaaa

--- src/config/index.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    DBURI: process.env.DBURI,
-    DBURIBACKUP: process.env.DBURIBACKUP,
-    PORT: process.env.PORT,
-    SECRET: process.env.SECRET,
-    SALTS: process.env.SALTS,
-    SECRETTOKEN: process.env.SECRETTOKEN
-}
--- src/routes/reports.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { reportController } from "../controllers/reports.js";
-import { authVerify } from "../middleware/index.js";
-
-export const reportRouter = Router()
-
-reportRouter.get('/', reportController.getReport)
-reportRouter.post('/', authVerify, reportController.createReport)
